5ef21c1e1f3eaab041693df3e415ca1d9407f9ba
Anton Lapanovich <lapanovich.anton@gmail.com>
2023-02-26 02:21:13 +0300

    add registration

--- server/index.js
+++ server/index.js
@@ -1,13 +1,16 @@
 const express = require ("express")
 const mongoose = require("mongoose")
 const config = require("config")
-
-const app = express()
+const authRouter = require("./routes/auth.routes")
+const app = express() // server creation from express
 const PORT = config.get('serverPort')
-const start = async () => {
+
+app.use(express.json()) // по умолчанию express не может распарсить json-строку, это необходимо указать явно
+app.use("/api/auth", authRouter)
+const start = async () => { // connection to the database is an async process
     try {
         mongoose.connect(config.get("dbUrl"))
-        app.listen(PORT, () => {
+        app.listen(PORT, () => { // running a server
             console.log('Server started on port', PORT)
         })
     } catch (e) {
--- server/models/User.js
+++ server/models/User.js
@@ -1,13 +1,14 @@
 const {Schema, model, ObjectId} = require("mongoose")
 
 
-const User = new Schema({
+const User = new Schema({ // mongoose default creates id
     email: {type: String, required: true, unique: true},
     password: {type: String, required: true},
     diskSpace: {type: Number, default: 1024**3*10},
     usedSpace: {type: Number, default: 0},
     avatar: {type: String},
-    files: [{type: ObjectId, ref: 'File'}]
+    files: [{type: ObjectId, ref: 'File'}] // массив, каждый объект которого имеет тип ObjectId
+    // и ссылается на сущность file
 })
 
 module.exports = model('User', User)
