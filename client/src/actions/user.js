b3f91d7001b511e3970a00c6e23a3fef52b33596
Anton-Lapanovich <71089904+Anton-Lapanovich@users.noreply.github.com>
2023-02-26 20:19:54 +0300

    add auth middleware and reg form

--- client/src/actions/user.js
+++ client/src/actions/user.js
@@ -1,6 +1,6 @@
 // request functions
 import axios from 'axios' // async http requests to the server
-
+import {setUser} from "../reducers/userReducer";
 export const registration = async (email, password) => { // async requests to the server
     try {
         // await for sequential execution
@@ -12,5 +12,34 @@ export const registration = async (email, password) => { // async requests to th
     } catch (e) {
         alert(e.response.data.message)
     }
+}
 
+export const login =  (email, password) => {
+    return async dispatch => {
+        try {
+            const response = await axios.post(`http://localhost:5000/api/auth/login`, {
+                email,
+                password
+            })
+            dispatch(setUser(response.data.user))
+            localStorage.setItem('token', response.data.token)
+        } catch (e) {
+            alert(e.response.data.message)
+        }
+    }
+}
+
+export const auth =  () => {
+    return async dispatch => {
+        try {
+            const response = await axios.get(`http://localhost:5000/api/auth/auth`,
+                {headers:{Authorization:`Bearer ${localStorage.getItem('token')}`}}
+            )
+            dispatch(setUser(response.data.user))
+            localStorage.setItem('token', response.data.token)
+        } catch (e) {
+            alert(e.response.data.message)
+            localStorage.removeItem('token')
+        }
+    }
 }
--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,12 +1,11 @@
-// contains routes that process user registration and authorization
 const Router = require("express");
 const User = require("../models/User")
 const bcrypt = require("bcryptjs")
 const config = require("config")
 const jwt = require("jsonwebtoken")
 const {check, validationResult} = require("express-validator")
-const router = new Router() // router object
-
+const router = new Router()
+const authMiddleware = require('../middleware/auth.middleware')
 
 router.post('/registration',
     [
@@ -19,29 +18,26 @@ router.post('/registration',
             if (!errors.isEmpty()) {
                 return res.status(400).json({message: "Uncorrect request", errors})
             }
-
-            const {email, password} = req.body;
-
+            const {email, password} = req.body
             const candidate = await User.findOne({email})
-
             if(candidate) {
                 return res.status(400).json({message: `User with email ${email} already exist`})
             }
             const hashPassword = await bcrypt.hash(password, 8)
             const user = new User({email, password: hashPassword})
             await user.save()
-            return res.json({message: "User was created"})
-
+            res.json({message: "User was created"})
         } catch (e) {
             console.log(e)
             res.send({message: "Server error"})
         }
     })
 
+
 router.post('/login',
-     async (req, res) => {
+    async (req, res) => {
         try {
-           const {email, password} = req.body
+            const {email, password} = req.body
             const user = await User.findOne({email})
             if (!user) {
                 return res.status(404).json({message: "User not found"})
@@ -50,6 +46,27 @@ router.post('/login',
             if (!isPassValid) {
                 return res.status(400).json({message: "Invalid password"})
             }
+            const token = jwt.sign({id: user.id}, config.get("secretKey"), {expiresIn: "1h"})
+            return res.json({
+                token,
+                user: {
+                    id: user.id,
+                    email: user.email,
+                    diskSpace: user.diskSpace,
+                    usedSpace: user.usedSpace,
+                    avatar: user.avatar
+                }
+            })
+        } catch (e) {
+            console.log(e)
+            res.send({message: "Server error"})
+        }
+    })
+
+router.get('/auth', authMiddleware,
+    async (req, res) => {
+        try {
+            const user = await User.findOne({_id: req.user.id})
             const token = jwt.sign({id: user.id}, config.get("secretKey"), {expiresIn: "1h"})
             return res.json({
                 token,
