a138f04f01794a0d74ae4d554efba63b86376f53
Anton-Lapanovich <71089904+Anton-Lapanovich@users.noreply.github.com>
2023-02-27 13:23:19 +0300

    add file upload

--- server/index.js
+++ server/index.js
@@ -1,14 +1,15 @@
 const express = require ("express")
 const mongoose = require("mongoose")
 const config = require("config")
+const fileUpload = require("express-fileupload")
 const authRouter = require("./routes/auth.routes")
 const fileRouter = require("./routes/file.routes")
 const app = express() // server creation from express
 const PORT = config.get('serverPort')
-// механизм безопасности, который позволяет ресурсу с одного домена обращаться на другой
-const corsMiddleware = require('./middleware/cors.middleware')
+const corsMiddleware = require('./middleware/cors.middleware') // механизм безопасности, который позволяет ресурсу с одного домена обращаться на другой
 
 
+app.use(fileUpload({}))
 app.use(corsMiddleware)
 app.use(express.json()) // по умолчанию express не может распарсить json-строку, это необходимо указать явно
 app.use("/api/auth", authRouter)
