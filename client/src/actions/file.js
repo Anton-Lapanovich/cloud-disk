c7b42e14e0a6ca2ed925e0226635388a4750729b
Anton-Lapanovich <71089904+Anton-Lapanovich@users.noreply.github.com>
2023-02-27 23:52:52 +0300

    add search files

--- client/src/actions/file.js
+++ client/src/actions/file.js
@@ -1,16 +1,30 @@
 import axios from 'axios'
 import {addFile, deleteFileAction, setFiles} from "../reducers/fileReducer";
 import {addUploadFile, changeUploadFile, showUploader} from "../reducers/uploadReducer";
+import {hideLoader, showLoader} from "../reducers/appReducer";
 
-export function getFiles(dirId) {
+export function getFiles(dirId, sort) {
     return async dispatch => {
         try { // сheck for an empty ID
-            const response = await axios.get(`http://localhost:5000/api/files${dirId ? '?parent='+dirId : ''}`, {
+            dispatch(showLoader())
+            let url = `http://localhost:5000/api/files`
+            if (dirId) {
+                url = `http://localhost:5000/api/files?parent=${dirId}`
+            }
+            if (sort) {
+                url = `http://localhost:5000/api/files?sort=${sort}`
+            }
+            if (dirId && sort) {
+                url = `http://localhost:5000/api/files?parent=${dirId}&sort=${sort}`
+            }
+            const response = await axios.get(url, {
                 headers: {Authorization: `Bearer ${localStorage.getItem('token')}`}
-            })
+            });
             dispatch(setFiles(response.data))
         } catch (e) {
             alert(e.response.data.message)
+        } finally {
+            dispatch(hideLoader())
         }
     }
 }
@@ -45,8 +59,7 @@ export function uploadFile(file, dirId) {
             dispatch(addUploadFile(uploadFile))
             const response = await axios.post(`http://localhost:5000/api/files/upload`, formData, {
                 headers: {Authorization: `Bearer ${localStorage.getItem('token')}`},
-                onUploadProgress: progressEvent => {
-                    // obtaining file size using headers
+                onUploadProgress: progressEvent => { // obtaining file size using headers
                     const totalLength = progressEvent.lengthComputable ? progressEvent.total : progressEvent.target.getResponseHeader('content-length') || progressEvent.target.getResponseHeader('x-decompressed-content-length');
                     if (totalLength) {
                         uploadFile.progress = Math.round((progressEvent.loaded * 100) / totalLength)
@@ -60,6 +73,7 @@ export function uploadFile(file, dirId) {
         }
     }
 }
+
 
 export async function downloadFile(file) {
     const response = await fetch(`http://localhost:5000/api/files/download?id=${file._id}`,{
@@ -91,6 +105,23 @@ export function deleteFile(file) {
             alert(response.data.message)
         } catch (e) {
             alert(e?.response?.data?.message)
+        }
+    }
+}
+
+export function searchFiles(search) {
+    return async dispatch => {
+        try {
+            const response = await axios.get(`http://localhost:5000/api/files/search?search=${search}`,{
+                headers:{
+                    Authorization: `Bearer ${localStorage.getItem('token')}`
+                }
+            })
+            dispatch(setFiles(response.data))
+        } catch (e) {
+            alert(e?.response?.data?.message)
+        } finally {
+            dispatch(hideLoader())
         }
     }
 }
