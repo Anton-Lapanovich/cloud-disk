01eda54cc510cf615df0bb3318c90cefacc547af
Anton-Lapanovich <71089904+Anton-Lapanovich@users.noreply.github.com>
2023-02-27 13:34:40 +0300

    add client file upload

--- client/src/actions/file.js
+++ client/src/actions/file.js
@@ -3,7 +3,7 @@ import {addFile, setFiles} from "../reducers/fileReducer";
 
 export function getFiles(dirId) {
     return async dispatch => {
-        try { // проверка на пустой ID
+        try {
             const response = await axios.get(`http://localhost:5000/api/files${dirId ? '?parent='+dirId : ''}`, {
                 headers: {Authorization: `Bearer ${localStorage.getItem('token')}`}
             })
@@ -13,6 +13,7 @@ export function getFiles(dirId) {
         }
     }
 }
+
 export function createDir(dirId, name) {
     return async dispatch => {
         try {
@@ -23,6 +24,33 @@ export function createDir(dirId, name) {
             }, {
                 headers: {Authorization: `Bearer ${localStorage.getItem('token')}`}
             })
+            dispatch(addFile(response.data))
+        } catch (e) {
+            alert(e.response.data.message)
+        }
+    }
+}
+
+export function uploadFile(file, dirId) {
+    return async dispatch => {
+        try {
+            const formData = new FormData()
+            formData.append('file', file)
+            if (dirId) {
+                formData.append('parent', dirId)
+            }
+            const response = await axios.post(`http://localhost:5000/api/files/upload`, formData, {
+                headers: {Authorization: `Bearer ${localStorage.getItem('token')}`},
+                onUploadProgress: progressEvent => {
+                    // получение размера файла при помощи заголовков
+                    const totalLength = progressEvent.lengthComputable ? progressEvent.total : progressEvent.target.getResponseHeader('content-length') || progressEvent.target.getResponseHeader('x-decompressed-content-length');
+                    console.log('total', totalLength)
+                    if (totalLength) {
+                        let progress = Math.round((progressEvent.loaded * 100) / totalLength)
+                        console.log(progress)
+                    }
+                }
+            });
             dispatch(addFile(response.data))
         } catch (e) {
             alert(e.response.data.message)
